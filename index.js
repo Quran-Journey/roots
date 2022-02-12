e49eff6e16a3454636a859858a932a04a6f430ea
osama <osama.w.hafez@gmail.com>
2022-02-12 16:49:29 -0500

    clean up

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const express = require("express");
 const { google } = require("googleapis");
-// const { MongoClient } = require("mongodb");
-
-/**
- * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
- * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
- */
-// const mongoConnectionString =
-//   "mongodb+srv://FalafelTest:3hGvTbgRmuTi9SSS@cluster0.yu591.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const app = express();
 
@@ -16,33 +8,17 @@ app.get("/sheet", async (req, res) => {
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
-
-  // Create client instance for auth
-
   const client = await auth.getClient();
-
-  // Create instance of google sheets api
-
   const googleSheets = google.sheets({ version: "v4", auth: client });
-
-  // Get metadata out of spreadsheet
+  
   const spreadsheetId = "13mENKJN1bRv2vt2-cwLhZZ02cVxdml2Zxm0REaRsJ4c";
   const metadata = await googleSheets.spreadsheets.get({
-    auth, // the same as writing auth: auth
+    auth,
     spreadsheetId,
   });
-
-  // Get a list of all of the metadata for the sheets in a spreadsheet (i.e. sheet name for each sheet)
   let sheets = metadata.data.sheets;
-
-  // For example, we can get the title of the first sheet in a
-  // spreadsheet by accessing the first index in the list
   let first_ayah = sheets[1].properties.title;
-  console.log(sheets);
-  console.log(first_ayah);
-  // Read rows from a spreadsheet
-
-  const rows = await googleSheets.spreadsheets.values.get({
+  let rows = await googleSheets.spreadsheets.values.get({
     auth,
     spreadsheetId,
     range: first_ayah,
@@ -56,24 +32,14 @@ app.get("/doc", async (req, res) => {
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/documents",
   });
-
-  // Create client instance for auth
-
   const client = await auth.getClient();
-
-  // Create instance of google docs api
-
   const googleDocs = google.docs({ version: "v1", auth: client });
 
-  // Get metadata out of DOCUMENT
   const documentId = "1O9Iqxu95szS4gowJZdGyMJTlP8LghsuJQhDqWUMojSY";
-
   const docmetadata = await googleDocs.documents.get({
-    auth, // the same as writing auth: auth
+    auth,
     documentId,
   });
-
-  console.log(docmetadata);
 
   res.send(docmetadata.data);
 });
