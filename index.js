9872303445bb889241f9a59261dd825c1dd053d5
osama <osama.w.hafez@gmail.com>
2022-02-12 16:47:16 -0500

    remove monogo stuff

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const { google } = require("googleapis");
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
 
 /**
  * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
  * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
  */
-const mongoConnectionString =
-  "mongodb+srv://FalafelTest:3hGvTbgRmuTi9SSS@cluster0.yu591.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const mongoConnectionString =
+//   "mongodb+srv://FalafelTest:3hGvTbgRmuTi9SSS@cluster0.yu591.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const app = express();
 
 app.get("/sheet", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
-    scopes: ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/documents"],
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
-
-  // Note, when connecting, ensure that your IP address is whitelisted by mongodb Atlas
-  const mongoClient = new MongoClient(mongoConnectionString);
-
-  await mongoClient.connect().catch((e) => console.log(e));
-
-  await listDatabases(mongoClient);
 
   // Create client instance for auth
 
@@ -31,7 +24,6 @@ app.get("/sheet", async (req, res) => {
   // Create instance of google sheets api
 
   const googleSheets = google.sheets({ version: "v4", auth: client });
-  const googleDocs = google.docs({ version: "v1", auth: client });
 
   // Get metadata out of spreadsheet
   const spreadsheetId = "13mENKJN1bRv2vt2-cwLhZZ02cVxdml2Zxm0REaRsJ4c";
@@ -39,7 +31,6 @@ app.get("/sheet", async (req, res) => {
     auth, // the same as writing auth: auth
     spreadsheetId,
   });
-
 
   // Get a list of all of the metadata for the sheets in a spreadsheet (i.e. sheet name for each sheet)
   let sheets = metadata.data.sheets;
@@ -60,31 +51,23 @@ app.get("/sheet", async (req, res) => {
   res.send(rows.data);
 });
 
-
 app.get("/doc", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
-    scopes: ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/documents"],
+    scopes: "https://www.googleapis.com/auth/documents",
   });
-
-  // Note, when connecting, ensure that your IP address is whitelisted by mongodb Atlas
-  const mongoClient = new MongoClient(mongoConnectionString);
-
-  await mongoClient.connect().catch((e) => console.log(e));
-
-  await listDatabases(mongoClient);
 
   // Create client instance for auth
 
   const client = await auth.getClient();
 
-  // Create instance of google sheets api
+  // Create instance of google docs api
 
   const googleDocs = google.docs({ version: "v1", auth: client });
 
   // Get metadata out of DOCUMENT
   const documentId = "1O9Iqxu95szS4gowJZdGyMJTlP8LghsuJQhDqWUMojSY";
-  
+
   const docmetadata = await googleDocs.documents.get({
     auth, // the same as writing auth: auth
     documentId,
@@ -95,20 +78,4 @@ app.get("/doc", async (req, res) => {
   res.send(docmetadata.data);
 });
 
-async function listDatabases(client) {
-  const databasesList = await client.db().admin().listDatabases();
-
-  console.log(databasesList.databases);
-}
-
-async function insertSurahIntoDB(client) {
-  const databasesList = await client.db("surahs");
-
-  console.log(databasesList.databases);
-}
-
 app.listen(1377, (req, res) => console.log("running on port 1377"));
-
-// Note to self: mongo stores things in BSON format which is a binary representation of JSON
-// So when you hear the word "document", you can just think of a JSON object
-// Every document has an _id which is unique. This will be created automatically but can be defined manually.
