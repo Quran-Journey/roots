c51516e011b9702a1b2e8ba9198a51bd700cf165
osama <osama.w.hafez@gmail.com>
2022-02-18 21:56:54 -0500

    prepare files for modular development

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
 const express = require("express");
-const { google } = require("googleapis");
+const { parseDocument } = require("./parsing");
 
 const app = express();
 
-app.get("/sheet", async (req, res) => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-  const client = await auth.getClient();
-  const googleSheets = google.sheets({ version: "v4", auth: client });
-  
-  const spreadsheetId = "13mENKJN1bRv2vt2-cwLhZZ02cVxdml2Zxm0REaRsJ4c";
-  const metadata = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
-  let sheets = metadata.data.sheets;
-  let first_ayah = sheets[1].properties.title;
-  let rows = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range: first_ayah,
-  });
-
-  res.send(rows.data);
-});
-
-app.get("/doc", async (req, res) => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/documents",
-  });
-  const client = await auth.getClient();
-  const googleDocs = google.docs({ version: "v1", auth: client });
-
-  const documentId = "1O9Iqxu95szS4gowJZdGyMJTlP8LghsuJQhDqWUMojSY";
-  const docmetadata = await googleDocs.documents.get({
-    auth,
-    documentId,
-  });
-
-  res.send(docmetadata.data);
+/**
+ * Long story short:
+ * Google will give us a JSON object containing every single details about the document
+ * We will extract all of the information we want and store it in a new JSON object.
+ * This JSON object will be formatted for our needs.
+ * 
+ * example document_id: 11wJIGkCKk9GMyOdNlcQMnlJ-7K-VXmb1mA0QsFzG79k
+ */
+app.get("/docs/:document_id", async (req, res) => {
+  res.send(await parseDocument(req.params.document_id));
 });
 
 app.listen(1377, (req, res) => console.log("running on port 1377"));
--- parsing.js
+++ parsing.js
@@ -1,3 +1,59 @@
-// This file's purpose is to parse documents
-// https://developers.google.com/docs/api/reference/rest/v1/documents
+const { google } = require("googleapis");
 
+// Our main funciton that executes parsing of an entire document
+async function parseDocument(documentId) {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/documents",
+  });
+  const client = await auth.getClient();
+  const googleDocs = google.docs({ version: "v1", auth: client });
+
+  const docmetadata = await googleDocs.documents.get({
+    auth,
+    documentId,
+  });
+
+  // Add new_section index extracted_data
+  let extracted = { ...docmetadata.data, new_section: 0 };
+
+  // For each ayah
+  parseLinguistics(extracted);
+  parseTafasir(extracted);
+  parseOthers(extracted);
+  return extracted;
+}
+
+/**
+ * A function that focuses on parsing linguistics.
+ * We parse from the beginning of a linguistics section up until a tafsir section.
+ * Note that we will return the newly formatted data which will also contain an index "new_section" for the beginning of the next section.
+ */
+function parseLinguistics(original_data) {
+  let new_format;
+  return { original_data, new_format };
+}
+
+/**
+ * A function that focuses on parsing tafasir.
+ * We parse from the beginning of a tafsir section up until a comment section.
+ * Note that we will return the newly formatted data which will also contain an index "new_section" for the beginning of the next section.
+ */
+function parseTafasir(original_data) {
+  let new_format;
+  return { original_data, new_format };
+}
+
+/**
+ * A function that focuses on parsing comments.
+ * We parse from the beginning of a comment section up until a new ayah section.
+ * Note that we will return the newly formatted data which will also contain an index "new_section" for the beginning of the next section.
+ */
+function parseOthers(original_data) {
+  let new_format;
+  return { original_data, new_format };
+}
+
+// Feel free to add any helper functions below this comment but above the module exports.
+
+module.exports = { parseDocument: parseDocument };
