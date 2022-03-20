c56be896f93f846ece81341d5a5cf06737b87a54
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2022-03-20 01:32:01 -0400

    Finished setting up docker

--- roots/insert_data.py
+++ roots/insert_data.py
@@ -6,8 +6,8 @@ try:
     connection = psycopg2.connect(user="qj",
                                   password="Yatathakar123!",
                                   host="127.0.0.1",
-                                  port="5434:5432",
-                                  database="postgres_db")
+                                  port="5434",
+                                  database="quranJourney")
 
     # Create a cursor to perform database operations
     cursor = connection.cursor()
@@ -16,9 +16,13 @@ try:
     print(connection.get_dsn_parameters(), "\n")
     # Executing a SQL query
     cursor.execute("SELECT version();")
+
+    fetch_rows =  "SELECT * FROM quran_text;"
+    cursor.execute(fetch_rows)
     # Fetch result
     record = cursor.fetchone()
     print("You are connected to - ", record, "\n")
+    
 
 except (Exception, Error) as error:
     print("Error while connecting to PostgreSQL", error)
@@ -28,32 +32,3 @@ finally:
         connection.close()
         print("PostgreSQL connection is closed")
 
-
-data = pd.read_excel("./cleaned_data")
-
-def fetch_ayah():
-    """
-    Fetch and Ayah using quran.com's API
-    """
-
-
-def parse_word_from_verse():
-    """
-    Create an ordered list containing the words from a verse.
-    """
-
-
-def scrape_root():
-    """
-    Return the root letters of word by scraping https://corpus.quran.com/wordbyword.jsp.
-    """
-
-
-def main():
-    """
-    Put everything together in this function
-    """
-
-
-if __name__ == "__main__":
-    main()
