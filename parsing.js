20e9afc445e685603d904480c95021fee0c0a6ed
osama <osama.w.hafez@gmail.com>
2022-03-22 11:49:26 -0400

    find horizontal rule

--- parsing.js
+++ parsing.js
@@ -2,26 +2,42 @@ const { google } = require("googleapis");
 
 // Our main funciton that executes parsing of an entire document
 async function parseDocument(documentId) {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/documents",
-  });
-  const client = await auth.getClient();
-  const googleDocs = google.docs({ version: "v1", auth: client });
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/documents",
+    });
+    const client = await auth.getClient();
+    const googleDocs = google.docs({ version: "v1", auth: client });
 
-  const docmetadata = await googleDocs.documents.get({
-    auth,
-    documentId,
-  });
+    const docmetadata = await googleDocs.documents.get({
+        auth,
+        documentId,
+    });
 
-  // Add new_section index extracted_data
-  let extracted = { ...docmetadata.data, new_section: 0 };
+    // Add new_section index extracted_data
+    let extracted = { ...docmetadata.data, new_section: 0 };
 
-  // For each ayah
-  parseLinguistics(extracted);
-  parseTafasir(extracted);
-  parseOthers(extracted);
-  return extracted;
+    parseVerse(extracted);
+
+    // For each ayah
+    parseLinguistics(extracted);
+    parseTafasir(extracted);
+    parseOthers(extracted);
+    return extracted;
+}
+
+function parseVerse(original_data) {
+    let new_format;
+    let content = original_data.body.content;
+    // console.log(body)
+    verses = content.filter((line) => {
+        line?.paragraph?.elements.forEach((element, index) => {
+            if (element?.horizontalRule) {
+                console.log(element.horizontalRule, index);
+            }
+        });
+    });
+    return { original_data, new_format };
 }
 
 /**
@@ -30,8 +46,8 @@ async function parseDocument(documentId) {
  * Note that we will return the newly formatted data which will also contain an index "new_section" for the beginning of the next section.
  */
 function parseLinguistics(original_data) {
-  let new_format;
-  return { original_data, new_format };
+    let new_format;
+    return { original_data, new_format };
 }
 
 /**
@@ -40,8 +56,8 @@ function parseLinguistics(original_data) {
  * Note that we will return the newly formatted data which will also contain an index "new_section" for the beginning of the next section.
  */
 function parseTafasir(original_data) {
-  let new_format;
-  return { original_data, new_format };
+    let new_format;
+    return { original_data, new_format };
 }
 
 /**
@@ -50,8 +66,8 @@ function parseTafasir(original_data) {
  * Note that we will return the newly formatted data which will also contain an index "new_section" for the beginning of the next section.
  */
 function parseOthers(original_data) {
-  let new_format;
-  return { original_data, new_format };
+    let new_format;
+    return { original_data, new_format };
 }
 
 // Feel free to add any helper functions below this comment but above the module exports.
