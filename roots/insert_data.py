83f6352a0e160d02aad10ace9b77678524f0a1e2
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2022-03-27 16:32:13 -0400

    Data insertion completed for 2/3 tables

--- roots/insert_data.py
+++ roots/insert_data.py
@@ -7,8 +7,7 @@ from psycopg2 import Error
 # Read in the data file that we need to insert
 df = pd.read_excel("/Users/tahir/Desktop/Github/documents/roots/Data/Cleaned_Root_letters.xlsx")
 
-# Prep data for the RootWords Table 
-# Get only the unique root words
+# Prep Data for RootWords Table 
 lst = list(df['Root_Letters'])
 lst = set(lst)
 
@@ -16,61 +15,87 @@ lst = set(lst)
 df_roots = pd.DataFrame(lst, columns=['Root_Words'])
 df_roots = df_roots.reset_index()
 
-#########################
-# ROOT WORDS TABLE DATA #
-#########################
+# df_roots["Root_Words"] = lst
+df_roots["Root_Words"] = df_roots["Root_Words"].values.astype(str)
 
-# Extract the data we need from the dataframe
-data = []
+# Put the data in the format the db accepts
+root_data = []
 for row in df_roots.index:
     arabic_text = df_roots.loc[row,"Root_Words"]
     unique_id = row
-    data.append((row, arabic_text))
+    root_data.append((row, arabic_text))
 
-# Define parameters for the database as global variables
-dbHost = '127.0.0.1'
-dbPort = 5434
-dbUser = 'qj'
-dbPassword= 'Yatathakar123!'
-dbName = 'quranJourney'
+# Prep Data for the ArabicWord Table
+arabic_df = df.drop(['ID', 'Transliteration'], axis=1)
+arabic_df = arabic_df.drop_duplicates()
+arabic_df["primary_key"] = list(range(2000,19623)) 
 
+# Merge the df to get the RootWords PK (called "index")
+arabic_words_df = arabic_df.merge(df_roots, how='inner', left_on='Root_Letters', right_on='Root_Words')
+arabic_words_df = arabic_words_df.drop(['Root_Letters', 'Root_Words'], axis=1)
+
+# Rename the index column to RootID to make it clear
+arabic_words_df.rename(columns = {'index':'RootID'}, inplace = True)
+
+# Put the dataframe into the format for postgres sql insertion 
+arabic_words_data = []
+for index, row in arabic_words_df.iterrows():
+    arabic_words_data.append( (row['primary_key'], row['ARABIC'], row['RootID']) )
+
+# Connect to the Postgres SQL Database 
+try:         
+    # Define DB connection parameters 
+    dbHost = '127.0.0.1'
+    dbPort = 5434
+    dbUser = 'qj'
+    dbPassword= 'Yatathakar123!'
+    dbName = 'quranJourney'
+
+    # connect to the PostgreSQL database
+    connection = psycopg2.connect(user=dbUser,
+                              password=dbPassword,
+                              host=dbHost,
+                              port=dbPort,
+                              database=dbName)
+    cursor = connection.cursor()
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+    
+# Create functions to insert root words and arabic words data     
 def insert_root_words(root_words_data):
-    try:
-        # connect to the PostgreSQL database
-        connection = psycopg2.connect(user=dbUser,
-                                  password=dbPassword,
-                                  host=dbHost,
-                                  port=dbPort,
-                                  database=dbName)
-        cursor = connection.cursor()
-
-        postgres_insert_query = """ INSERT INTO RootWords (RootID, RootWord) VALUES (%s ,%s)"""
-        counter = 0
-        for row in root_words_data:
-            record_to_insert = row
-            cursor.execute(postgres_insert_query, record_to_insert)
-            connection.commit()
-            counter += cursor.rowcount
-        print(counter, "records inserted successfully into rootWords table")
-
-    except (Exception, psycopg2.Error) as error:
-        print("Failed to insert record into rootwords table", error)
-
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-            
+    """ Insert Root Words Data into the POSTGRES DB """
+    postgres_insert_query = """ INSERT INTO RootWords (RootID, RootWord) VALUES (%s ,%s)"""
+    counter = 0
+    for row in root_words_data:
+        record_to_insert = row
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+        counter += cursor.rowcount
+    print(counter, "records inserted successfully into rootWords table")
     return counter
 
-# Call this function to insert the data inside the DB
-insert_root_words(data)
+def insert_arabic_text(arabic_text_data):
+    """ Insert Arabic Text Data into the POSTGRES DB """
+    postgres_insert_query = """ INSERT INTO ArabicWord (WordID, Word, RootID) VALUES (%s ,%s, %s)"""
+    counter = 0
+    for row in arabic_text_data:
+        record_to_insert = row
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+        counter += cursor.rowcount
+    print(counter, "records inserted successfully into ArabicWord table")
+    return counter
 
-############################
-# ARABIC WORDS TEXT TABLE  #
-############################ 
+if __name__ == '__main__':
 
+    # Insert the root_words data 
+    insert_root_words(root_data)
 
+    # Insert the arabic words data 
+    insert_arabic_text(arabic_words_data)
 
+    # Close the DB connection 
+    cursor.close()
+    connection.close()
+    print("PostgreSQL connection is closed")
