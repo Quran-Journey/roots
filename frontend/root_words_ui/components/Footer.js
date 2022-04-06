ba1210b77ed4a3ba9c258d18f1101ef6de3f4052
*Zeina* <zeina.adi@mail.utoronto.ca>
2022-04-06 06:06:34 -0400

    Fixing Navbar icons and positioning of Footer

--- frontend/root_words_ui/components/Footer.js
+++ frontend/root_words_ui/components/Footer.js
@@ -1,14 +1,15 @@
-import {Typography, Grid, Card, Paper, Button, Container, BottomNavigation, Box, BottomNavigationAction, AppBar, Toolbar, IconButton}  from '@mui/material';
-import {Mail, DeveloperMode} from '@mui/icons-material';
+import {Typography, Grid}  from '@mui/material';
 
 export default function Footer() {
     return (
     <div>
-    <Box position="relative" align="center" sx = {{pt : 10 }} style={{width:"100%", bottom: 0}}>
-        <Grid align="center">
-        <Typography> Powered by Quran Journey </Typography>   
-        </Grid>
-    </Box>
+    <Grid container style={{bottom: 0, position:"absolute", textAlign:"center", paddingBottom:10}}>
+      <Grid item xs={12}>
+        <Typography> Powered by Quran Journey </Typography>
+      </Grid>
+    </Grid>
     </div>
+
+    
   );
 }
