b1997d076d717b0dda48324c4178dd2713238d6f
osama <osama.w.hafez@gmail.com>
2022-05-21 13:36:44 -0400

    api allows for getting chapters, verses, and root words

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const https = require("https");
 const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
-require('dotenv').config()
+const roots = require("./routes/roots");
+require("dotenv").config();
 
 var port = 3001;
 
@@ -11,32 +12,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(async (req, res, next) => {
-    console.log(`\nEndpoint Hit: ${req.method} ${req.originalUrl}\n`);
-    next();
+  console.log(`\nEndpoint Hit: ${req.method} ${req.originalUrl}\n`);
+  next();
 });
 
-if (process.env.NODE_ENV == "production") {
-    // This sets the options for https so that it finds the ssl certificates
-    var privateKey = fs.readFileSync(
-        "/etc/letsencrypt/live/offlinequran.org-0001/privkey.pem"
-    );
-    var certificate = fs.readFileSync(
-        "/etc/letsencrypt/live/offlinequran.org-0001/cert.pem"
-    );
-    var chain = fs.readFileSync(
-        "/etc/letsencrypt/live/offlinequran.org-0001/fullchain.pem"
-    );
-    const httpsOptions = {
-        cert: certificate,
-        key: privateKey,
-        ca: chain,
-    };
+app.use("/api", roots);
 
-    var httpsServer = https.createServer(httpsOptions, app).listen(port, () => {
-        console.log("Serving on https");
-    });
+if (process.env.NODE_ENV == "production") {
+  // This sets the options for https so that it finds the ssl certificates
+  var privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/offlinequran.org-0001/privkey.pem"
+  );
+  var certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/offlinequran.org-0001/cert.pem"
+  );
+  var chain = fs.readFileSync(
+    "/etc/letsencrypt/live/offlinequran.org-0001/fullchain.pem"
+  );
+  const httpsOptions = {
+    cert: certificate,
+    key: privateKey,
+    ca: chain,
+  };
+
+  var httpsServer = https.createServer(httpsOptions, app).listen(port, () => {
+    console.log("Serving on https");
+  });
 } else if (process.env.NODE_ENV == "development") {
-    app.listen(port, () => {
-        console.log("Listening on port " + port);
-    });
+  app.listen(port, () => {
+    console.log("Listening on port " + port);
+  });
 }
