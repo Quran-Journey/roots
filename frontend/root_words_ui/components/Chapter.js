be18001aa124c433116651d3392b41574175751c
*Zeina* <zeina.adi@mail.utoronto.ca>
2022-05-21 07:15:39 -0400

    Restructure code (continued)

---
+++ frontend/root_words_ui/components/Chapter.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import  {getVerseOptions} from "./Verse";
+import {FormHelperText, FormControl, Select, MenuItem } from '@mui/material/';
+import {InputLabel} from '@mui/material/';
+import { getChapter } from ".././mockAPI";
+
+export default function Chapter(props) {;
+
+    const handleChapterChange = (event) => {
+        props.setChapter(event.target.value);
+        let ver = getVerseOptions(event.target.value);
+        let verse_options = Object.keys(ver).map((c) => { return ver[c].verse_list; });
+        props.setVerseOptions(verse_options)
+      };
+
+    let chapt = {};
+    for (var i = 1; i <= 3; i++) {  //replace 3 by 114 when API completed
+        let name = getChapter(i).name 
+        let chapt_list = [];
+        chapt_list.push(<MenuItem key={i} value={i}>Chapter {i} : {name}</MenuItem>);  
+        chapt[`chapter_${i}`] = {chapt_list};
+    }
+    let chapter_options = Object.keys(chapt).map((c) => { return chapt[c].chapt_list; });
+
+    return(
+    <div>
+        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="chapter-select-required-label">Chapter</InputLabel>
+            <Select
+            labelId="chapter-select-required-label"
+            id="chapter-select-required"
+            value={props.currentChapter}
+            label="Chapter"
+            onChange={handleChapterChange}
+            >
+            {chapter_options}
+            </Select>
+            <FormHelperText>Required</FormHelperText>
+        </FormControl>
+    </div>
+);
+}
---
+++ frontend/root_words_ui/components/Verse.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {FormHelperText, FormControl, Select, MenuItem} from '@mui/material/';
+import {InputLabel, Button, Typography, Box, Card, CardContent, Grid} from '@mui/material/';
+import { getChapter, getNumberofVerses, getChapterVerses, getVerse, getRootWords} from ".././mockAPI";
+
+export function getVerseOptions(chapter){
+    let verses = {};
+    let verse_count = getNumberofVerses(chapter)   
+    for (var i = 1; i <= verse_count; i++){
+        let verse_list = [];
+        verse_list.push(<MenuItem key={i} value={i}>Verse {i}</MenuItem>); 
+        verses[`verse_${i}`] = {verse_list}; 
+    }
+    return verses
+}
+
+export default function Verse(props) {
+    
+    return(
+        <div>
+            <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="verse-select-required-label">Verse</InputLabel>
+                <Select
+                labelId="verse-select-required-label"
+                id="verse-select-required"
+                value={props.currentVerse}
+                label="Verse"
+                onChange={props.handleVerseChange}>
+                {props.verseOptions}
+                </Select>
+                <FormHelperText>Required</FormHelperText>
+            </FormControl>
+        </div>
+    );
+    }
