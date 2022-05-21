44f09a2d31fad5be9912c96e8f70a3b8c088b11f
*Zeina* <zeina.adi@mail.utoronto.ca>
2022-05-21 02:36:16 -0400

    Handling verse options after chapter selection & connecting root words display to selection buttons

--- frontend/root_words_ui/components/Form.js
+++ frontend/root_words_ui/components/Form.js
@@ -5,14 +5,14 @@ import RootWordsDisplay from "./RootWordsDisplay";
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import { getChapters, getNumberofVerses, getChapterVerses, getVerse, getRootWords, getChapterName} from ".././mockAPI";
+import { getChapter, getNumberofVerses, getChapterVerses, getVerse, getRootWords} from ".././mockAPI";
 
 
 export default function Form() {
   //---------- -----------------Displaying Chapter Options -----------------------//
   let chapt = {};
   for (var i = 1; i <= 3; i++) {  //replace 3 by 114 when API completed
-      let name = getChapterName(i)
+      let name = getChapter(i).name 
       let chapt_list = [];
       chapt_list.push(<MenuItem key={i} value={i}>Chapter {i} : {name}</MenuItem>);  
       chapt[`chapter_${i}`] = {chapt_list};
@@ -21,29 +21,38 @@ export default function Form() {
   let chapter_options = Object.keys(chapt).map((c) => { return chapt[c].chapt_list; });
   
   //----------------------------Displaying Verse Options -----------------------// 
-  let verses = {};
-  let verse_count = getNumberofVerses(2)   ///testing using: chapter 2 --> FIGURE OUT 
-  for (var i = 1; i <= verse_count; i++){
-     let verse_list = [];
-     verse_list.push(<MenuItem key={i} value={i}>Verse {i}</MenuItem>); 
-     verses[`verse_${i}`] = {verse_list};
+  function getVerseOptions(chapter){
+    let verses = {};
+    let verse_count = getNumberofVerses(chapter)   ///testing using: chapter 2 --> FIGURE OUT 
+    for (var i = 1; i <= verse_count; i++){
+      let verse_list = [];
+      verse_list.push(<MenuItem key={i} value={i}>Verse {i}</MenuItem>); 
+      verses[`verse_${i}`] = {verse_list}; 
+    }
+    return verses
   }
-     let verse_options = Object.keys(verses).map((c) => { return verses[c].verse_list; }); 
+
 
   //----------------------------------------------------------------------------//
 
   const [currentChapter, setChapter] = React.useState('');
   const [currentVerse, setVerse] = React.useState('');
+  const [verseOptions, setVerseOptions] = React.useState('');
 
   const handleChapterChange = (event) => {
     setChapter(event.target.value);
+    let ver = getVerseOptions(event.target.value);
+    let verse_options = Object.keys(ver).map((c) => { return ver[c].verse_list; });
+    setVerseOptions(verse_options)
   };
 
-  // change this to be dependent on chapter change
   const handleVerseChange = (event) => {
     setVerse(event.target.value)
   };
-   
+
+  React.useEffect(() => {
+    (currentChapter === '') ? setVerse('') : handleVerseChange})
+
   //----------------------------Find Root Button Action---------------------------------------//
   const [showRootWords, setShowRootWords] = React.useState(false)
   const onClickFindRoot = () => {setShowRootWords(true)}
@@ -59,17 +68,16 @@ export default function Form() {
     setVerse(currentVerse + 1)
   };
 
-  // React.useEffect(() => console.log(currentVerse))  ---> to test if pre/next buttons work
   //------------------------------------------------------------------------------------------//
 
     return (
     <div>
       <Box display="flex" justifyContent="center">  
       <FormControl required sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-required-label">Chapter</InputLabel>
+        <InputLabel id="chapter-select-required-label">Chapter</InputLabel>
         <Select
-          labelId="demo-simple-select-required-label"
-          id="demo-simple-select-required"
+          labelId="chapter-select-required-label"
+          id="chapter-select-required"
           value={currentChapter}
           label="Chapter"
           onChange={handleChapterChange}
@@ -79,14 +87,14 @@ export default function Form() {
         <FormHelperText>Required</FormHelperText>
       </FormControl>
       <FormControl required sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-required-label">Verse</InputLabel>
+        <InputLabel id="verse-select-required-label">Verse</InputLabel>
         <Select
-          labelId="demo-simple-select-required-label"
-          id="demo-simple-select-required"
+          labelId="verse-select-required-label"
+          id="verse-select-required"
           value={currentVerse}
           label="Verse"
           onChange={handleVerseChange}>
-          {verse_options}
+          {verseOptions}
         </Select>
         <FormHelperText>Required</FormHelperText>
 
@@ -99,11 +107,17 @@ export default function Form() {
       </Box>
 
       <Grid container direction="row" justifyContent="center" alignItems="center" display="inline-flex">
+
         <Grid item xs={3}><Button type="submit" variant="contained" color="inherit" onClick={getPrevVerse}> 
-        <IconButton className=" fa-solid fa-angle-left"></IconButton>Previous Verse</Button></Grid>
-        <Grid item xs={6} className="roots-display">{ showRootWords ? <RootWordsDisplay /> : null }</Grid> 
+        <IconButton className=" fa-solid fa-angle-left"></IconButton>Previous Verse</Button>
+        </Grid>
+
+        <Grid item xs={6} className="roots-display">{ showRootWords ? <RootWordsDisplay currentChapter={currentChapter} currentVerse={currentVerse} /> : null }</Grid> 
+
         <Grid item xs={3}><Button type="submit" variant="contained" color="inherit" onClick={getNextVerse}> 
-        Next Verse<IconButton className=" fa-solid fa-angle-right"></IconButton></Button></Grid>
+        Next Verse<IconButton className=" fa-solid fa-angle-right"></IconButton></Button>
+        </Grid>
+
       </Grid>
       
 
