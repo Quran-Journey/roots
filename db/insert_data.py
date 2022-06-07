28674ee4a31a736cd4df59feef2c8566761d8e86
osama <osama.w.hafez@gmail.com>
2022-06-07 19:36:29 -0400

    make sure db dump and info is set to docker container and not local

--- db/insert_data.py
+++ db/insert_data.py
@@ -92,10 +92,10 @@ for index, row in TextToWords_df.iterrows():
 try:         
     # Define DB connection parameters 
     dbHost = '127.0.0.1'
-    dbPort = 5432
+    dbPort = 5434
     dbUser = 'qj'
     dbPassword= 'Yatathakar123!'
-    dbName = 'quranjourney'
+    dbName = 'quranJourney'
 
     # connect to the PostgreSQL database
     connection = psycopg2.connect(user=dbUser,
