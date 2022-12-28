6f497e4532b94af1bff43a0c5faa05ec2f6b2d2c
osama <osama.w.hafez@gmail.com>
2022-12-28 16:18:43 -0500

    use a different database

--- db/backup.sh
+++ db/backup.sh
@@ -10,4 +10,4 @@ docker-compose up --build -d;
 python3 insert_data.py;
 cd ../model; node insertMeanings.js;
 
-docker exec -i db_postgres_alone_1 /bin/bash -c "PGPASSWORD=Yatathakar123! pg_dump --username qj quranJourney" > ./filled.sql
+docker exec -i db_postgres_alone_1 /bin/bash -c "PGPASSWORD=Yatathakar123! pg_dump --username qj qj_roots" > ./filled.sql
